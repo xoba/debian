bc84a8bf1601cf0b9a32223ba6cc00bbbc725bca
mike andrews <mra@xoba.com>
2014-03-16 09:17:10 -0400

    auto install of debian, working on it

--- create.sh
+++ create.sh
@@ -15,13 +15,21 @@ vboxmanage modifyvm $1 --nic1 bridged --bridgeadapter1 eth1
 vboxmanage modifyvm $1 --nictype1 82545EM
 
 # dvd drive
+wget -N http://cdimage.debian.org/cdimage/daily-builds/daily/arch-latest/amd64/iso-cd/debian-testing-amd64-netinst.iso
+wget -N http://cdimage.debian.org/cdimage/daily-builds/daily/arch-latest/amd64/iso-cd/MD5SUMS.small
+export OK=`md5sum -c MD5SUMS.small | grep "OK" | wc -l`
+if [ $OK != 1 ]
+then
+    echo "bad md5sum for debian-testing-amd64-netinst.iso"
+    exit
+fi
+
 vboxmanage storagectl $1 --name "IDE" --add ide
-wget http://cdimage.debian.org/cdimage/daily-builds/daily/arch-latest/amd64/iso-cd/debian-testing-amd64-netinst.iso
-VBoxManage storageattach $1 --storagectl "IDE" --type dvddrive --port 0 --device 0 --medium /home/mra/Desktop/debian-testing-amd64-netinst.iso
+vboxmanage storageattach $1 --storagectl "IDE" --type dvddrive --port 0 --device 0 --medium /home/mra/Desktop/debian-testing-amd64-netinst.iso
 
 # hard drive
 vboxmanage storagectl $1 --name "SATA" --add sata
-VBoxManage createhd --filename /big/$1.vdi --size 40960
+vboxmanage createhd --filename /big/$1.vdi --size 40960
 vboxmanage storageattach $1 --storagectl "SATA" --port 0 --device 0 --type hdd --medium /big/$1.vdi
 
 # start it
