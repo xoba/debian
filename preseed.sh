1c62c3cca2552dd0781986582e2c584dbf9a76a5
mike andrews <mra@xoba.com>
2014-03-17 07:11:03 -0400

    got an rc.local installed

--- preseed.sh
+++ preseed.sh
@@ -2,11 +2,7 @@
 
 # following https://wiki.debian.org/DebianInstaller/Preseed/EditIso on 2014-03-16
 
-if [ -d cd ]
-then
-    chmod -R 777 cd
-    sudo rm -rf cd
-fi
+sudo rm -rf cd loopdir irmod
 
 sudo mkdir loopdir
 sudo mount -r -o loop debian-testing-amd64-netinst.iso loopdir
@@ -15,16 +11,18 @@ rsync -a -H --exclude=TRANS.TBL loopdir/ cd
 sudo umount loopdir
 rm -rf loopdir
 
-chmod -R u+w cd
+sudo cp rc.local cd
+sudo chmod -R u+w cd
 
-rm -rf irmod
+sudo chmod uog+x cd/rc.local
+
 mkdir irmod
 cd irmod
 gzip -d < ../cd/install.amd/initrd.gz | sudo cpio --extract --make-directories --no-absolute-filenames
 cp ../preseed.cfg preseed.cfg
 find . | cpio -H newc --create | gzip -9 > ../cd/install.amd/initrd.gz
 cd ../
-sudo rm -fr irmod/
+sudo rm -rf irmod
 
 cd cd
 cat > isolinux/isolinux.cfg <<EOF
@@ -37,8 +35,11 @@ label install
 	kernel /install.amd/vmlinuz
 	append vga=788 initrd=/install.amd/initrd.gz -- quiet 
 EOF
-md5sum `find -follow -type f` > md5sum.txt
+md5sum `find -L . -type f` > md5sum.txt
 cd ..
 
-genisoimage -quiet -o preseeded.iso -r -J -l -no-emul-boot -boot-load-size 4 -boot-info-table -b isolinux/isolinux.bin -z -iso-level 4 -c isolinux/boot.cat ./cd
+sudo chown -R root cd
+sudo chgrp -R root cd
+
+sudo genisoimage -quiet -o preseeded.iso -r -J -l -no-emul-boot -boot-load-size 4 -boot-info-table -b isolinux/isolinux.bin -z -iso-level 4 -c isolinux/boot.cat ./cd
 
