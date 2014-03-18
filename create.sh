e9d16e1b0699931fcbcac7a70ccd3dfdce6054c2
mike andrews <mra@xoba.com>
2014-03-18 05:58:10 -0400

    control disk size too

--- create.sh
+++ create.sh
@@ -4,8 +4,9 @@ VMNAME=test
 MEMORY=8192
 CPUS=4
 RUN=boot.sh
+DISK=40960
 
-options=':n:m:c:r:help'
+options=':n:m:c:r:d:help'
 while getopts $options option
 do
     case $option in
@@ -13,7 +14,8 @@ do
         m  )    MEMORY=$OPTARG;;
         c  )    CPUS=$OPTARG;;
         r  )    RUN=$OPTARG;;
-	h  )    echo "./vbox.sh -n <vmname> -m <memory> -c <cpus> -r <boot script>"; exit;;
+        d  )    DISK=$OPTARG;;
+	h  )    echo "./vbox.sh -n <vmname> -m <memory> -c <cpus> -r <boot script> -d <mb of disk>"; exit;;
     esac
 done
 
@@ -32,8 +34,8 @@ vboxmanage storageattach $VMNAME --storagectl "IDE" --type dvddrive --port 0 --d
 
 # hard drive
 vboxmanage storagectl $VMNAME --name "SATA" --add sata
-vboxmanage createhd --filename /big/$VMNAME.vdi --size 40960
-vboxmanage storageattach $VMNAME --storagectl "SATA" --port 0 --device 0 --type hdd --medium /big/$VMNAME.vdi
+vboxmanage createhd --filename $VMNAME.vdi --size $DISK
+vboxmanage storageattach $VMNAME --storagectl "SATA" --port 0 --device 0 --type hdd --medium $VMNAME.vdi
 
 # start it
 vboxmanage startvm $VMNAME --type headless
---
+++ readme.md
@@ -0,0 +1,8 @@
+utilities for running debian in virtualbox
+
+    ./create.sh -n test -m 4096 -c 4 -d 20480
+
+creates a debian server instance with 4096mb memory, 4 cpu's, and 20gb of disk
+
+
+
