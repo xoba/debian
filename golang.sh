1db5d8afa2283d8d44425a34ff223e5759930e93
mike andrews <mra@xoba.com>
2014-03-29 16:23:58 -0400

    hmmm, still trying to get auto boot back after latest debian changes

--- golang.sh
+++ golang.sh
@@ -2,7 +2,7 @@
 
 # build golang if it doesn't already exist
 
-if [ -d /root/go ]; 
+if [ ! -d /root/go ]; 
 then 
     cd /root
     source <(curl https://raw.github.com/xoba/goinit/master/buildgo.sh) 2>&1 | tee golog.txt
